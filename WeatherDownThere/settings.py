04171c5a557af6e91759fc5bcd37251a7b80994f
Sean Broomfield <srbroomfield@aol.com>
2019-08-10 21:43:56 -0400

    Creation of home page and card element.

--- WeatherDownThere/settings.py
+++ WeatherDownThere/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'weather.apps.WeatherConfig',
     'django.contrib.staticfiles',
 ]
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'WeatherDownThere.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), 'WeatherDownThere/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -119,6 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
