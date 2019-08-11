c454ea2a9ffeb8838bdaa1c0be68a70ebdbea4fa
Sean Broomfield <srbroomfield@aol.com>
2019-08-11 03:43:16 -0400

    Detail Page done

--- WeatherDownThere/urls.py
+++ WeatherDownThere/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from weather import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('event/', include('weather.urls')),
 ]
--- weather/views.py
+++ weather/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'weather/home.html')
+
+
+def detail(request):
+    return render(request, 'weather/detail.html')
